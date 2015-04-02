ec76c6fbb0cdbcaa97e30081c33f6ec58452822d
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2015-04-01 18:15:00 -0700

    Initial Commit

---
+++ Ransom/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ransom
+//
+//  Created by Rodrigo Carballo on 4/1/15.
+//  Copyright (c) 2015 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
